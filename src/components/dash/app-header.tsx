f0716f4723e9cd88926405207990748ca20b02b8
yanqi.zong <yzong@turo.com>
2025-03-19 09:33:27 -0700

    feat: add app-header user avatar

--- src/components/dash/app-header.tsx
+++ src/components/dash/app-header.tsx
@@ -10,21 +10,13 @@ import {
   DropdownMenu,
   Avatar,
 } from '@heroui/react';
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { querySessionOptions } from '~/lib/queries/auth';
+import { AcmeIcon } from '~/components/icons/social';
 
 export function AppHeader() {
+  const { data } = useSuspenseQuery(querySessionOptions);
+
   return (
     <Navbar
       classNames={{
@@ -32,8 +24,10 @@ export function AppHeader() {
       }}
     >
       <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        <AcmeIcon />
+        <p className="font-bold text-inherit">
+          {import.meta.env.VITE_APP_NAME}
+        </p>
       </NavbarBrand>
 
       <NavbarContent as="div" justify="end">
@@ -61,15 +55,15 @@ export function AppHeader() {
               as="button"
               className="transition-transform"
               color="secondary"
-              name="Jason Hughes"
+              name={data?.user.name}
               size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              src={data?.user.image ?? undefined}
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
               <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
+              <p className="font-semibold">{data?.user.email}</p>
             </DropdownItem>
             <DropdownItem key="settings">My Settings</DropdownItem>
             <DropdownItem key="team_settings">Team Settings</DropdownItem>
